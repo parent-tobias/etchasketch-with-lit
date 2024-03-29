2caaaf7c2251a02fe1d6a98f79b6285702c598d5
tobyPlaysTheUke <parent.tobias@gmail.com>
2024-03-28 20:13:45 -0400

    Commented index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { DrawingMode } from './components/DrawingMode/DrawingMode';
 export {ActionsPanel} from './components/ActionsPanel/ActionsPanel';
 
 
+// Dynamically add coloring handlers to the actions panel, creating the mode buttons.
 const handlers = [
 	{title: 'Default', handler: (cell)=>cell.style.backgroundColor="#bbb"},
 	{title: 'Black', handler: (cell)=>cell.style.backgroundColor='#333'},
@@ -10,13 +11,15 @@ const handlers = [
 ]
 document.querySelector('actions-panel').modes = handlers;
 
+// setup the default so *something* happens.
 document.querySelector("etch-grid").onMouseover = handlers[0].handler;
 
+
+// listen for changes to both drawing mode buttons, and size change.
+// When either happens, tell the etch-grid about it and let it handle it itself.
 document.querySelector('actions-panel').addEventListener("mode-change", (evt)=>
   document.querySelector('etch-grid').onMouseover=evt.detail.handler
 )
-document.querySelector('actions-panel').addEventListener("size-change", (evt)=>{
-	console.log(evt.detail);
-  document.querySelector('etch-grid').size=evt.detail.size;
-}
+document.querySelector('actions-panel').addEventListener("size-change", (evt)=>
+  document.querySelector('etch-grid').size=evt.detail.size
 )
